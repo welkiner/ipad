1f48f3d4951b7d749fdd27b1d3489295da5dc8c6
welkiner <tianliang.black@gmail.com>
2016-05-12 19:37:56 +0800

    init

---
+++ ipad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ipad
+//
+//  Created by tl on 16/5/12.
+//  Copyright © 2016年 welkiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

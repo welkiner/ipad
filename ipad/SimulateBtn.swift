73e1fe37512dacd3705bf52609eb937941c6cbb0
tl <tl@iMac.local>
2017-07-20 14:21:30 +0800

    btn color

--- ipad/SimulateBtn.swift
+++ ipad/SimulateBtn.swift
@@ -16,11 +16,12 @@ class SimulateBtn: UIButton {
         didSet {
             layer.cornerRadius = cornerRadius
             layer.masksToBounds = cornerRadius > 0
-            backgroundColor = UIColor.red.withAlphaComponent(0.4)
         }
     }
     override func awakeFromNib() {
-        
+        super.awakeFromNib()
+        self.setBackgroundImage(imageColor(UIColor.black.withAlphaComponent(0.7)), for: .highlighted)
+        self.setBackgroundImage(imageColor(UIColor.black.withAlphaComponent(0.7)), for: .disabled)
     }
 }
 @IBDesignable
@@ -31,10 +32,20 @@ class PageBtn: UIButton {
         didSet {
             layer.cornerRadius = cornerRadius
             layer.masksToBounds = cornerRadius > 0
-            backgroundColor = UIColor.red.withAlphaComponent(0.4)
         }
     }
     override func awakeFromNib() {
-        
+        super.awakeFromNib()
     }
+}
+
+func imageColor(_ color: UIColor) -> UIImage {
+    let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+    UIGraphicsBeginImageContext(rect.size)
+    let context = UIGraphicsGetCurrentContext()
+    context?.setFillColor(color.cgColor)
+    context?.fill(rect)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return image!
 }

6b74ea37c7e583a4874e38be4f89ba72cfff4fa8
tl <tl@iMac.local>
2017-07-22 13:46:21 +0800

    init

---
+++ ipad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ipad
+//
+//  Created by tl on 2017/7/22.
+//  Copyright © 2017年 welkiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ipad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ipad
+//
+//  Created by tl on 2017/7/22.
+//  Copyright © 2017年 welkiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

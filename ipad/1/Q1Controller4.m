fbb6b87b3d44fbfaf9022594940732102dfc5359
welkiner <tianliang.black@gmail.com>
2017-08-21 00:25:57 +0800

    a

--- ipad/1/Q1Controller4.m
+++ ipad/1/Q1Controller4.m
@@ -9,7 +9,70 @@
 #import "Q1Controller4.h"
 #import "FinishController.h"
 #import "AnswerData.h"
-@interface Q1Controller4 ()<UITableViewDelegate,UITableViewDataSource>
+#import <Masonry.h>
+@interface Cell : UITableViewCell
+@property (strong,nonatomic)UILabel *label;
+@property (strong,nonatomic)UILabel *label2;
+@end
+@implementation Cell
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(nullable NSString *)reuseIdentifier{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self.label = [UILabel new];
+        self.label.textColor = [UIColor blackColor];
+        self.label.font = [UIFont systemFontOfSize:24];
+        self.label.backgroundColor = [UIColor whiteColor];
+        [self addSubview:self.label];
+        [self.label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self);
+            make.left.equalTo(self);
+            make.bottom.equalTo(self);
+            make.width.mas_equalTo(48);
+        }];
+        self.label2 = [UILabel new];
+        self.label2.textColor = [UIColor blackColor];
+        self.label2.font = [UIFont systemFontOfSize:24];
+        self.label2.backgroundColor = [UIColor whiteColor];
+        [self addSubview:self.label2];
+        [self.label2 mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self);
+            make.right.equalTo(self);
+            make.bottom.equalTo(self);
+            make.left.equalTo(self.label.mas_right);
+        }];
+    }
+    return self;
+}
+@end
+@interface Cell2 : Cell
+@property (strong,nonatomic)UITextField *textField;
+@end
+@implementation Cell2
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(nullable NSString *)reuseIdentifier{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        [self.label2 removeFromSuperview];
+        self.label2 = nil;
+        
+        self.textField = [UITextField new];
+        self.textField.textColor = [UIColor blackColor];
+        self.textField.font = [UIFont systemFontOfSize:24];
+        self.textField.backgroundColor = [UIColor whiteColor];
+        [self addSubview:self.textField];
+        [self.textField mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self);
+            make.right.equalTo(self);
+            make.bottom.equalTo(self);
+            make.left.equalTo(self.label.mas_right);
+        }];
+    }
+    return self;
+}
+@end
+
+
+@interface Q1Controller4 ()<UITableViewDelegate,UITableViewDataSource>{
+    NSMutableArray *_listArray;
+    NSString *_EStr;
+}
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
@@ -18,9 +81,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _listArray = @[@"A",@"B",@"C",@"D",@"E"].mutableCopy;
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    [self.tableView registerClass:[Cell class] forCellReuseIdentifier:@"cell"];
+    [self.tableView registerClass:[Cell2 class] forCellReuseIdentifier:@"cell2"];
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     [self.tableView setEditing:YES animated:YES];
     // Do any additional setup after loading the view.
@@ -37,20 +102,30 @@
 //    FinishController *con = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"FinishController"];
 //    [self.navigationController pushViewController:con animated:YES];
 }
+
+static NSDictionary *__dic(){
+    return @{@"A":@"儿童ECMO套包",@"B":@"心室辅助套包",@"C":@"Mecc套包",@"D":@"自定义体外循环套包",@"E":@"其他请注明"};
+}
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
     NSUInteger fromRow = [sourceIndexPath row];
     NSUInteger toRow = [destinationIndexPath row];
 }
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 44;
+    return 47;
 }
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 5;
+    return _listArray.count;
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    cell.backgroundColor = [UIColor grayColor];
-    cell.textLabel.text = @"AAAAAAAA";
+    if ([_listArray[indexPath.row] isEqualToString:@"E"]) {
+        Cell2 *cell = [tableView dequeueReusableCellWithIdentifier:@"cell2" forIndexPath:indexPath];
+        cell.textField.text = __dic()[_listArray[indexPath.row]];
+        cell.label.text = [NSString stringWithFormat:@"%@ ：",_listArray[indexPath.row]];
+        return cell;
+    }
+    Cell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.label2.text = __dic()[_listArray[indexPath.row]];
+    cell.label.text = [NSString stringWithFormat:@"%@ ：",_listArray[indexPath.row]];
     return cell;
 }
 
